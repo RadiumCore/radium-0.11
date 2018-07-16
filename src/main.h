ded5b17f91d1738aecaaaa547893cbd503e5879d
Unknown <Justinj100@gmail.com>
2018-07-16 19:06:06 -0400

    Fix block speed
    Current block speed is one per every 62 to 63 seconds. Should be closer to 60

--- src/main.h
+++ src/main.h
@@ -104,7 +104,12 @@ inline int64_t FutureDriftV1(int64_t nTime) { return nTime + 10 * 60; }
 inline int64_t FutureDriftV2(int64_t nTime) { return nTime + 2 * 60; }
 inline int64_t FutureDrift(int64_t nTime, int nHeight) { return IsProtocolV2(nHeight) ? FutureDriftV2(nTime) : FutureDriftV1(nTime); }
 
-inline unsigned int GetTargetSpacing(int nHeight) { return IsProtocolV2(nHeight) ? 60 : 60; }
+inline unsigned int GetTargetSpacing(int nHeight) 
+{
+    if (IsProtocolV4(nHeight))
+        return 57;
+    return 60;
+}
 
 extern CScript COINBASE_FLAGS;
 extern CCriticalSection cs_main;
