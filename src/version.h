bfa3a4a537afe720a0f65a33e23c366b6c55ddbb
Unknown <Justinj100@gmail.com>
2018-07-16 19:07:28 -0400

    Set minimum peer version

--- src/version.h
+++ src/version.h
@@ -30,13 +30,16 @@ static const int DATABASE_VERSION = 70510;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 99007;
+static const int PROTOCOL_VERSION = 99008;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 99007;
+
+// disconnect from peers older than this proto version if nheight is after DEV_SUBSIDY_BLOCK_HEIGHT
+static const int DEV_SUBSIDY_PROTOCOL_VERSION = 99008;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
